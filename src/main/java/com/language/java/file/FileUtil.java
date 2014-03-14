385e649a12a8a1205eb1b1d7dfd9207130909171
sanen <zhangkehuait@163.com>
2014-03-14 15:37:26 +0800

    add some methods about how to create file, write content to file and read file content

--- src/main/java/com/language/java/file/FileUtil.java
+++ src/main/java/com/language/java/file/FileUtil.java
@@ -1,30 +1,95 @@
 package com.language.java.file;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 
 public class FileUtil {
 
     /**
      * create a file
      */
+    @SuppressWarnings("unused")
     public void createFile() {
+        File file = new File("example.txt");
     }
 
+    /**
+     * create a file by specific filename
+     */
+    @SuppressWarnings("unused")
     public void createFile(String fileName) {
-
-    }
-
-    public void createFile(String fileName, String filePath) {
+        File file = new File(fileName);
 
     }
 
     /**
-     * write content to file
+     * create a file by specific filename and filepath
      */
+    @SuppressWarnings("unused")
+    public void createFile(String fileName, String filePath) {
+        File file = new File(filePath + "/" + fileName);
+    }
+
+    /**
+     * write content to file by BufferedWriter
+     */
+    public static void writeContentToFile() {
+        Writer writer = null;
+
+        try {
+            writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("config/filename.txt"), "utf-8"));
+            writer.write("Something");
+        } catch (IOException ex) {
+            // report
+        } finally {
+            try {
+                writer.close();
+            } catch (Exception ex) {
+            }
+        }
+    }
+
+    /**
+     * This method is used to write content to file by BufferedWriter 1) create a new file 2) install output object to
+     * write content 3) write content
+     */
+    public static void writeContent() {
+        String text = "Hello world";
+        try {
+            File file = new File("example.txt");
+            BufferedWriter output = new BufferedWriter(new FileWriter(file));
+            output.write(text);
+            output.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * write content to file by FileOutputStream
+     */
+    public static void writeFileContent() {
+        try {
+            // What ever the file path is.
+            File statText = new File("config/statsTest.txt");
+            FileOutputStream is = new FileOutputStream(statText);
+            OutputStreamWriter osw = new OutputStreamWriter(is);
+            Writer w = new BufferedWriter(osw);
+            w.write("POTATO!!!");
+            w.close();
+        } catch (IOException e) {
+            System.err.println("Problem writing to the file statsTest.txt");
+        }
+    }
 
     /**
      * change file content
@@ -43,17 +108,15 @@ public class FileUtil {
      */
 
     /**
-     * read file content then print it to console 
-     * 1) load file 
-     * 2) read file content for each line 
-     * 3) print each line content, you also could save line content to StringBuffer or StringBuilder then print it
+     * read file content then print it to console 1) load file 2) read file content for each line 3) print each line
+     * content, you also could save line content to StringBuffer or StringBuilder then print it
      */
     @SuppressWarnings("unused")
-    private String readFileContent() {
+    private static String readFileContent() {
         final String methodName = "getDeviceDetailFromLocal";
         final StringBuffer buffer = new StringBuffer();
         try {
-            final FileInputStream inputStream = new FileInputStream("globalResources/portlet/config/deviceconfig/server.xml");
+            final FileInputStream inputStream = new FileInputStream("config/server.xml");
             final BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
             String line = reader.readLine();
             while (line != null) {
@@ -68,6 +131,7 @@ public class FileUtil {
         }
 
         final String deviceDetail = buffer.toString();
+        System.out.println(deviceDetail);
         return deviceDetail;
     }
 
@@ -75,4 +139,12 @@ public class FileUtil {
      * append A file content to B file
      */
 
+    public static void main(String[] args) {
+        // test read file
+        // readFileContent();
+
+        // writeContentToFile();
+
+        writeContent();
+    }
 }
